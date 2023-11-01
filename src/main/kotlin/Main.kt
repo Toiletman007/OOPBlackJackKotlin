fb3f9fb3862549ff4dba1c47d258f7bdd906e4c5
Wc man <Quertical5@gmail.com>
2023-11-01 21:18:08 +0100

    Start

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var player1Points: Int =
+}
+
+enum class CardValues(values: Int, aceValues: Int) {
+    ACE(11, 1), ONE(1, 1),
+    TWO(2, 2), THREE(3, 3),
+    FOUR(4, 4), FIVE(5, 5),
+    SIX(6, 6), SEVEN(7, 7),
+    EIGHT(8, 8), NINE(9, 9),
+    TEN(10, 10), JACK(10, 10),
+    QUEEN(10, 10), KING(10, 10)
+}
